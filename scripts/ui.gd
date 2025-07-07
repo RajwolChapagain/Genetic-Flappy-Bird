25e3020c263c02ad8076843cac0284a797b8c3ca
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-07 11:28:52 -0400

    Pause/Play on UI button toggle

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -5,3 +5,11 @@ func _ready() -> void:
 
 func contract_all_docks() -> void:
 	get_tree().call_group("dock", "contract_dock")
+
+
+func _on_play_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		get_tree().paused = false
+	else:
+		get_tree().paused = true
+	
