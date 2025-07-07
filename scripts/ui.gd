df9a408d0973ad1de98d435ac3ff0885b971d37b
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-07 11:35:56 -0400

    Set engine time scale based on time scale slider

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,18 +2,20 @@ extends Control
 
 func _ready() -> void:
 	call_deferred("contract_all_docks")
+	%TimeScaleSlider.value = Engine.time_scale
 
 func contract_all_docks() -> void:
 	get_tree().call_group("dock", "contract_dock")
-
-
+	
 func _on_play_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		get_tree().paused = false
 	else:
 		get_tree().paused = true
 	
-
 func _on_reset_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+func _on_time_scale_slider_value_changed(value: float) -> void:
+	Engine.time_scale = value
