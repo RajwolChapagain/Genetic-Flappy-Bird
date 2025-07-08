f936428392c0332d9629f9e8d98e9d83b764f994
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-08 10:46:14 -0400

    Create a warning icon scene

---
+++ scripts/warning_icon.gd
@@ -0,0 +1,18 @@
+extends Control
+
+enum types { REQUIRES_NEXT_GENERATION, REQUIRES_RESTART }
+@export var type: types
+
+@export var requires_next_gen_sprite: Texture2D
+@export var requires_restart_sprite: Texture2D
+		
+func show_icon() -> void:
+	if type == types.REQUIRES_NEXT_GENERATION:
+		%TextureRect.texture = requires_next_gen_sprite
+	elif type == types.REQUIRES_RESTART:
+		%TextureRect.texture = requires_restart_sprite
+	
+	%TextureRect.visible = true
+
+func hide_icon() -> void:
+	%TextureRect.visible = false
