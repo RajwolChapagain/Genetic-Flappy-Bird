a2c4b9ce962959e5b653b2945d62ff604310331a
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-07-10 13:24:44 -0400

    Add tooltip to warning icon

--- scripts/warning_icon.gd
+++ scripts/warning_icon.gd
@@ -5,6 +5,12 @@ enum types { REQUIRES_NEXT_GENERATION, REQUIRES_RESTART }
 
 @export var requires_next_gen_sprite: Texture2D
 @export var requires_restart_sprite: Texture2D
+
+func _ready() -> void:
+	if type == types.REQUIRES_NEXT_GENERATION:
+		tooltip_text = "Requires next generation"
+	elif type == types.REQUIRES_RESTART:
+		tooltip_text = "Requires restart"
 		
 func show_icon() -> void:
 	if type == types.REQUIRES_NEXT_GENERATION:
